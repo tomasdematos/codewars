9abdf7da8bc2ce6aec687d9df4cd72500a59d875
Tomás de Matos <tomas.dm404@gmail.com>
2026-02-08 01:23:07 -0300

    Add solution - CodeHub

---
+++ 5-kyu/greed-is-good/greed-is-good.js
@@ -0,0 +1,24 @@
+​
+const SCORING_RULES = {
+  1: { triplet: 1000, individual: 100 },
+  2: { triplet: 200,  individual: 0   },
+  3: { triplet: 300,  individual: 0   },
+  4: { triplet: 400,  individual: 0   },
+  5: { triplet: 500,  individual: 50  },
+  6: { triplet: 600,  individual: 0   },
+};
+​
+const calculatePoints = (face, amount) => {
+  const {triplet, individual} = SCORING_RULES[face];
+​
+  return Math.floor(amount/3)*triplet + (amount%3)*individual;
+​
+}
+​
+function score(dice) {
+  const scores = new Array(6).fill(0)
+  dice.forEach((item) => scores[item-1]++)
+  return scores.reduce((acc, val, i) =>  acc+(calculatePoints(i+1, val)),0)
+​
+}
+​
