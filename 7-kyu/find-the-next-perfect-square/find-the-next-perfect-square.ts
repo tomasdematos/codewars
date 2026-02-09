940553f3039e3686f4ab88a4c1c4104659b22c1f
Tomás de Matos <tomas.dm404@gmail.com>
2026-02-09 12:19:09 -0300

    Update solution - CodeHub

--- 7-kyu/find-the-next-perfect-square/find-the-next-perfect-square.ts
+++ 7-kyu/find-the-next-perfect-square/find-the-next-perfect-square.ts
@@ -1,9 +1,7 @@
 export function findNextSquare(sq:number):number {
-  if(!Number.isInteger(Math.sqrt(sq))) return -1
-  
-  let next = sq+1;
-  while(!Number.isInteger(Math.sqrt(next))) {
-    next++
-  }
-  return next;
+  const sqrt = Math.sqrt(sq);
+  if(!Number.isInteger(sqrt)) return -1;
+​
+  return (sqrt + 1) ** 2;
+​
 }
