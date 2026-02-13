7f0e5bd546a30adf880cc5001b3560faf41a2446
Tomás de Matos <tomas.dm404@gmail.com>
2026-02-13 00:10:33 -0300

    Update solution - CodeHub

--- 5-kyu/product-of-consecutive-fib-numbers/product-of-consecutive-fib-numbers.ts
+++ 5-kyu/product-of-consecutive-fib-numbers/product-of-consecutive-fib-numbers.ts
@@ -2,12 +2,11 @@ export const productFib = (prod:number): [number,number,boolean] => {
   let f1 = 1;
   let f2 = 0; 
   let aux = 0;
-  
+​
   while (f1 * f2 < prod) {
     aux = f1;
     f1 = f1+f2;
     f2 = aux; 
   }
-  if(f1*f2 === prod) return [f2,f1, true]
-  return [aux,f1,false]
+  return [f2,f1, f1*f2 === prod]
 }
