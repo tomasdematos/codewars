bde57b15fc544202bf55465be495863dc7f6e841
Tomás de Matos <tomas.dm404@gmail.com>
2026-07-13 17:15:44 -0300

    Update solution - CodeHub

--- 6-kyu/unique-in-order/unique-in-order.ts
+++ 6-kyu/unique-in-order/unique-in-order.ts
@@ -1,12 +1,4 @@
 export function uniqueInOrder (iterable: string | (string | number)[]):(string | number)[] {
-  const res: (string | number)[] = [];
-  
-  for (let i = 0; i < iterable.length; i++) {
-    if(res.length < 1 || res[res.length-1] !== iterable[i]) {
-      res.push(iterable[i]);
-    }
-  }
-  
-  return res;
+  return [...iterable].filter((i, index) => i !== iterable[index+1] )
 };
 ​
