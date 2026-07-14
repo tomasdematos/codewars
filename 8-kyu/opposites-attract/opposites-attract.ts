fe0f155d641356836b45a4767628a21ab5ffd674
Tomás de Matos <tomas.dm404@gmail.com>
2026-07-14 00:25:17 -0300

    Update solution - CodeHub

--- 8-kyu/opposites-attract/opposites-attract.ts
+++ 8-kyu/opposites-attract/opposites-attract.ts
@@ -1,5 +1,3 @@
 export function lovefunc(flower1: number, flower2: number): boolean {
-  const a = flower1%2 === 0;
-  const b = flower2%2 === 0;
-  return a && !b || !a && b
+  return flower1%2 !== flower2%2
 }
